65d63b0eb7e63f80f4989377c26dbfb88de149a5
DenisFlow <deo9flow@gmail.com>
2021-01-21 08:25:58 +0300

    final

--- app/src/main/java/com/example/pcalculator/MainActivity.java
+++ app/src/main/java/com/example/pcalculator/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.pcalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtEnter
+    private EditText edtEnterPercent, edtEnterNumber;
+    private Button btnCalculate;
+    private TextView txtResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
+        edtEnterNumber = findViewById(R.id.edtEnterNumber);
+        edtEnterPercent = findViewById(R.id.edtEnterPercent);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtResult = findViewById(R.id.txtResult);
 
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double result = Double.parseDouble(edtEnterNumber.getText().toString()) * (Double.parseDouble(edtEnterPercent.getText().toString()) / 100);
+                txtResult.setText(String.format("%f", result));
+
+            }
+        });
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-////        getMenuInflater().inflate(R.menu_main, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
+
 }
